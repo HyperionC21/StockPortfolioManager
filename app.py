1850d20ebf2aa168a9690204fb15132ca95c8fc2
Catalin <catalin.craita21@gmail.com>
2022-03-12 22:40:57 +0200

    Adding first draft of rest app

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 import requests
 from flask import Flask
-from flask import request
+from flask import request, render_template
 
 import backend
+import json
 
 app = Flask(__name__)
 
+@app.route('/', methods = ['GET', 'POST'])
+def home():
+    if request.args is not None:
+        form_args = dict(request.json)
+        print(form_args)
+    return render_template(r'security_form.html')
+
+if __name__ == "__main__":
+    app.run()
