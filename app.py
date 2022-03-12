4ca8dae04414d2e2604bb3eb58d97ac5ca87ad35
Catalin <catalin.craita21@gmail.com>
2022-03-12 14:23:30 +0200

    Adding central backend and tweaks on reporting

---
+++ app.py
@@ -0,0 +1,8 @@
+import requests
+from flask import Flask
+from flask import request
+
+import backend
+
+app = Flask(__name__)
+
---
+++ backend/reporting.py
@@ -0,0 +1,30 @@
+import base
+
+
+class Reporter:
+    def __init__(self, db_model, connector):
+        self._db_connector = connector
+        self._db_model = db_model
+
+    def get_portfolio_composition(self, portfolio_id : int, ref_date : str):
+        query = f'''
+            SELECT
+                t1.TICKER,
+                SUM(t1.AMOUNT) as N_SHARES,
+                '{ref_date}' as DT
+            FROM
+                'TRANSACTION' as t1
+            WHERE
+                1 = 1
+                AND t1.PORTFOLIO_ID = {portfolio_id}
+                AND DATE(t1.DATE) < DATE('{ref_date}')
+            GROUP BY
+                t1.TICKER 
+        '''
+
+        df = self._db_connector.read_query(query)
+        
+        return df
+
+
+
