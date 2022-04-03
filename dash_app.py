1a8bc223fb29cdb8b24d79c19c7bd32be3398272
Catalin <catalin.craita21@gmail.com>
2022-04-03 22:32:17 +0300

    Adding input forms

--- dash_app.py
+++ dash_app.py
@@ -6,10 +6,11 @@ import json
 from backend import base, reporting
 from utils import utils
 from datetime import datetime
+import flask
 
 
 # frontend
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 from dash import dash_table
 import plotly.express as px
 app = Dash(__name__)
@@ -37,14 +38,46 @@ def get_composition(ref_date, ref_cost):
     df_composition = df_composition.sort_values(by='VALUE', ascending=False)
     return df_composition
 
+def get_form(fields, id_):
+    def get_field(name):
+        return html.Div([
+                    html.Label(name),
+                    dcc.Input(name=name),
+                ],
+                style={'display': 'flex', 'flex-direction': 'row', 'width' : '1000'}
+            )
+
+    fields = list(map(lambda x: get_field(x), fields))
+
+    form_security = html.Form([
+        *fields,
+        html.Button('Submit', type='submit')
+    ], action=f'/post_{id_}', method='post', id=id_)
+
+    return form_security
+
+INPUT_FORM_FIELDS = {
+    'security' : ['TICKER', 'SECTOR', 'COUNTRY', 'FX', 'MARKET', 'SRC'],
+    'transaction' : ['TICKER', 'PRICE', 'FX', 'AMOUNT', 'DATE'],
+    'security_values' : ['TICKER', 'DATE', 'CLOSE']
+}
+
+gl_form_name = 'security'
+
 if __name__ == '__main__':
     
     START_DT = '2022-01-10'
     now_ = utils.date2str(datetime.now())
 
+    
     db_connector = base.BaseDBConnector('core.db')
     data_fetcher = base.DataFetcher(db_connector)
     reporter = reporting.Reporter(data_fetcher)
+
+    # table handlers
+    
+    
+    
 
     missing_data_getter = base.DBUpdater(db_conn=db_connector)
     _ = missing_data_getter.fetch_missing_fx(START_DT, now_)
@@ -69,7 +102,9 @@ if __name__ == '__main__':
     fig_profit_perc = px.bar(df_comp, x='TICKER', y='PROFIT%', color='TICKER', title='Percentage profit')
     fig_profit = px.bar(df_comp, x='TICKER', y='PROFIT', color='TICKER', title='Absolute profit')
     
+
     roi_div = html.Div(
+        id='page-1-content',
         children=[
             dcc.Graph(figure=fig_roi),
             html.Div([
@@ -77,15 +112,101 @@ if __name__ == '__main__':
                 dcc.Graph(figure=fig_cost_pie)
                 ], style={'display': 'flex', 'flex-direction': 'row'}
             ),
+            
             dcc.Graph(figure=fig_profit_perc),
             dcc.Graph(figure=fig_profit)
         ],
         style={'display': 'flex', 'flex-direction': 'column', 'width' : '1000'}
     )
 
-    app.layout = roi_div
+    index_page = html.Div([
+        dcc.Link('Go to statistics', href='/page-1'),
+        html.Br(),
+        dcc.Link('Go to security', href='/security_id'),
+        html.Br(),
+        dcc.Link('Go to transactions', href='/transaction_id'),
+        html.Br(),
+        dcc.Link('Go to security values', href='/security_values_id'),
+    ])
 
+    page_1_layout = html.Div([
+        roi_div,
+        dcc.Link('Go back to home', href='/'),
+    ])
 
+    security_form = html.Div([
+        get_form(INPUT_FORM_FIELDS['security'], 'security_id'),
+        dcc.Link('Go back to home', href='/')
+    ])
 
+    transaction_form = html.Div([
+        get_form(INPUT_FORM_FIELDS['transaction'], 'transaction_id'),
+        dcc.Link('Go back to home', href='/')
+    ])
+
+    security_values_form = html.Div([
+        get_form(INPUT_FORM_FIELDS['security_values'], 'security_values_id'),
+        dcc.Link('Go back to home', href='/')
+    ])
+
+    @callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+    def display_page(pathname):
+        if pathname == '/page-1':
+            return page_1_layout
+        elif pathname == '/security_id':
+            return security_form
+        elif pathname == '/transaction_id':
+            return transaction_form
+        elif pathname == '/security_values_id':
+            return security_values_form
+        else:
+            return index_page
+    
+    @app.server.route('/post_security_id', methods=['POST'])
+    def on_security_post():
+
+        db_connector = base.BaseDBConnector('core.db')
+        security_t_handler = base.TableHandler(db_connector, 'SECURITY', '')
+
+        data = dict(flask.request.form)
+
+        for k in data.keys():
+            data[k] = [data[k]]
+
+        security_t_handler.insert_val(data)
+        return flask.redirect('/')
+
+    @app.server.route('/post_transaction_id', methods=['POST'])
+    def on_transaction_post():
+
+        db_connector = base.BaseDBConnector('core.db')
+        transaction_t_handler = base.TableHandler(db_connector, 'TRANSACTION', '')
+        data = dict(flask.request.form)
+
+        for k in data.keys():
+            data[k] = [data[k]]
+        
+        transaction_t_handler.insert_val(data)
+        return flask.redirect('/')
+
+    @app.server.route('/post_security_values_id', methods=['POST'])
+    def on_security_values_post():
+        
+        db_connector = base.BaseDBConnector('core.db')
+        security_values_t_handler = base.TableHandler(db_connector, 'SECURITY_VALUES', '')
+        
+        data = dict(flask.request.form)
+        
+        for k in data.keys():
+            data[k] = [data[k]]
+
+        security_values_t_handler.insert_val(data)
+        return flask.redirect('/')
+
+    app.layout = html.Div([
+        dcc.Location(id='url', refresh=False),
+        html.Div(id='page-content')
+    ])
 
     app.run_server()
